339c05d5c22dcffc1067ecf8a1fb24fd47e8594a
Matthieu Penant <thieum22@hotmail.com>
2021-03-29 13:25:03 -0400

    repro with proxydll and consumer project

---
+++ Consumer/Consumer/Form1.cs
@@ -0,0 +1,15 @@
+﻿using ProxyDLL;
+using System.Windows.Forms;
+
+namespace Consumer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            CefSharp.OffScreen.CefSettings test = new Class1().Test();
+        }
+    }
+}
---
+++ ProxyDLL/ProxyDLL/Class1.cs
@@ -0,0 +1,15 @@
+﻿using CefSharp.OffScreen;
+using System;
+using System.IO;
+
+namespace ProxyDLL
+{
+    public class Class1
+    {
+        public CefSettings Test() => new CefSettings()
+        {
+            //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
+            CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+        };
+    }
+}
